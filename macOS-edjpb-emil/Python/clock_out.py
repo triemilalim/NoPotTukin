0034dff0dfd7ab6e2718a8b471105be5bf5f8c2f
Tri Emil Alim <triemilalim@gmail.com>
2020-09-07 20:19:51 +0700

    Initial commit for macOS-edjpb

---
+++ macOS-edjpb-emil/Python/clock_out.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from selenium import webdriver
+
+from time import sleep
+
+from selenium.webdriver.common.keys import Keys 
+
+print("Mulai clock out at", datetime.now())
+password = 'putyourpasswordhere'
+
+driver = webdriver.Safari()
+
+# Open the website
+driver.get('https://edjpb.kemenkeu.go.id/login.php')
+
+sleep(10)
+
+# Locate id and password
+id_box = driver.find_element_by_name('username')
+pass_box = driver.find_element_by_name('password')
+
+# Send login information
+id_box.send_keys('putyourNIPhere')
+pass_box.send_keys(password)  
+pass_box.send_keys(Keys.RETURN)
+
+sleep(10)
+
+driver.find_element_by_partial_link_text('[detil]').click()
+
+sleep(10)
+
+driver.find_element_by_link_text('Clock-out').click()
+
+sleep(10)
+
+driver.find_element_by_xpath("//button[1]").click()
+
+driver.quit()
+print("All done, self destructing at", datetime.now())
