12b6ed6c32e06706c2e760fccd08e34f3fcf8716
zarifin <zarifin212@gmail.com>
2020-09-11 08:19:38 +0700

    clockin, clockout, add task

---
+++ windows-zaenal/configExample.py
@@ -0,0 +1,7 @@
+url_nadine = 'https://office.kemenkeu.go.id/'
+absen_nadine = 'nadine/absen'
+task_nadine = 'nadine/mytask'
+url_edjpb = 'https://edjpb.kemenkeu.go.id/login.php'
+
+username = 'username'
+password = 'password'
