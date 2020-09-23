7de980442332306e9f546dfe773fd88b27751bdb
triemilalim <triemilalim@gmail.com>
2020-09-23 16:05:36 +0700

    Added new windows by Emil based on Zaenal's code

---
+++ windows-emil/config.py
@@ -0,0 +1,12 @@
+url_nadine = 'https://office.kemenkeu.go.id/'
+absen_nadine = 'nadine/absen'
+task_nadine = 'nadine/mytask'
+url_edjpb = 'https://edjpb.kemenkeu.go.id/login.php'
+
+# Username nadinde dan edjpb (harus sama)
+username = 'putyourNIPhere'
+# Password nadine dan edjpb (harus sama)
+password = 'putyourpasswordhere'
+
+# File lokal berisi task
+localTaskxls = 'C:\\NoPotTukin.git\\windows-emil\\task.xlsx'
